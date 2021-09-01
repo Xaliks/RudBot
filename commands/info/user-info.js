2d6303c577d030994571004d323d6a53865e1ebf
Xaliks <miskinmatvej77@gmail.com>
2021-09-01 15:14:28 +0300

    (commands/user-info): format

--- commands/info/user-info.js
+++ commands/info/user-info.js
@@ -43,47 +43,51 @@ module.exports = {
 			)
 			.setFooter("Дизайн JeggyBot");
 
-		const presence = member ? member.presence : bot.guilds.cache.map((guild) => guild.members.cache.get(user.id)).filter((m) => m)[0]?.presence;
+		const presence = member
+			? member.presence
+			: bot.guilds.cache.map((guild) => guild.members.cache.get(user.id)).filter((m) => m)[0]?.presence;
 		//Статус
 		//-----------------------------------------------------------------------------
 		const clientStatus = [];
 		let status;
 		let activity = "";
 		if (presence && presence.status != "offline") {
-		// Система
-		//-----------------------------------------------------------------------------
-		for (let cs in presence.clientStatus) {
-			clientStatus.push(
-				`${emoji[presence.clientStatus[cs]]} ${
-					{
-						desktop: "Компьютер",
-						web: "Сайт",
-						mobile: "Телефон",
-					}[cs]
-				}`,
-			);
-		}
-		//-----------------------------------------------------------------------------
-
-		// Статус
-		//-----------------------------------------------------------------------------
-		presence.activities.forEach((act) => {
-			if (!act) return;
-			if (act.id === "custom") status = act.state;
-			else {
-				activity += require("../../utils/gamesEmoji")(act);
+			// Система
+			//-----------------------------------------------------------------------------
+			for (let cs in presence.clientStatus) {
+				clientStatus.push(
+					`${emoji[presence.clientStatus[cs]]} ${
+						{
+							desktop: "Компьютер",
+							web: "Сайт",
+							mobile: "Телефон",
+						}[cs]
+					}`,
+				);
 			}
-		});
-		//-----------------------------------------------------------------------------
+			//-----------------------------------------------------------------------------
+
+			// Статус
+			//-----------------------------------------------------------------------------
+			presence.activities.forEach((act) => {
+				if (!act) return;
+				if (act.id === "custom") status = act.state;
+				else {
+					activity += require("../../utils/gamesEmoji")(act);
+				}
+			});
+			//-----------------------------------------------------------------------------
 		} else clientStatus.push(`${emoji.offline} Оффлайн`);
 		//----------------------------------------------------------------------------
 
 		if (member) {
 			if (user.id === message.author.id) member = message.member;
 			description += `\nПрисоединился: \`${
-				message.guild.members.cache.map((member) => member.joinedTimestamp).filter((t) => t).sort((a, b) => a - b).indexOf(
-					member.joinedTimestamp,
-				) + 1
+				message.guild.members.cache
+					.map((member) => member.joinedTimestamp)
+					.filter((t) => t)
+					.sort((a, b) => a - b)
+					.indexOf(member.joinedTimestamp) + 1
 			}\`/\`${message.guild.members.cache.size}\``;
 
 			//Роли
@@ -101,7 +105,8 @@ module.exports = {
 		if (activity != "") embed.addField("Активность:", activity, true);
 		if (clientStatus[0]) embed.addField("Статус:", clientStatus.join("\n"), true);
 		if (status) embed.addField("Пользовательский статус:", status, true);
-		if (embed.fields.findIndex(field => field.name.startsWith("**Роли (") || field.name === "\u200b") != -1) embed.fields.splice(embed.fields.length - 1, 0, embed.fields.splice(0, 1)[0]);
+		if (embed.fields.findIndex((field) => field.name.startsWith("**Роли (") || field.name === "\u200b") != -1)
+			embed.fields.splice(embed.fields.length - 1, 0, embed.fields.splice(0, 1)[0]);
 		else embed.addField("\u200b", "\u200b", false);
 
 		if (member) embed.addField("Зашел на сервер:", bot.utils.discordTime(member.joinedTimestamp), true);
