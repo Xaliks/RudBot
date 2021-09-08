f544b713b335183b9a15be98b330b64d33171be6
Xaliks <miskinmatvej77@gmail.com>
2021-09-08 17:22:15 +0300

    (functiona/findMember): получение юзера с ответа

--- functions/findMember.js
+++ functions/findMember.js
@@ -1,14 +1,16 @@
 module.exports = (message, user, author) => {
+	const mentions = message.mentions.members.first();
 	if (!user || user === "" || user === "@") {
+		if (mentions) return mentions;
 		if (author) return message.member;
 		return undefined;
 	}
 
 	user = user.toString().toLowerCase();
-	if (!message.mentions.members.first() && user.startsWith("@")) user = user.slice(1);
+	if (!mentions && user.startsWith("@")) user = user.slice(1);
 
 	const ret =
-		message.mentions.members.first() ||
+		mentions ||
 		message.guild.members.cache.get(user) ||
 		message.guild.members.cache.find((m) => m.user.tag.toLowerCase().startsWith(user)) ||
 		message.guild.members.cache.find((m) => m.user.username.toLowerCase().startsWith(user)) ||
