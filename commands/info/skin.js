32a69d98865c912107200af941a90ba1b996fbcd
Xaliks <miskinmatvej77@gmail.com>
2021-09-21 20:45:51 +0300

    (commands/skin): format

--- commands/info/skin.js
+++ commands/info/skin.js
@@ -8,16 +8,19 @@ module.exports = {
 	cooldown: 5,
 	usage: ["<Ник игрока>"],
 	async execute(message, args, bot) {
-		const data = await fetch(
-			`https://api.mojang.com/users/profiles/minecraft/${encodeURIComponent(args[0])}`,
-		).then((res) => res.json()).catch((e) => null);
+		const data = await fetch(`https://api.mojang.com/users/profiles/minecraft/${encodeURIComponent(args[0])}`)
+			.then((res) => res.json())
+			.catch((e) => null);
 
 		if (!data) return bot.utils.error(`Игрок \`${args[0]}\` не найден!`, this, message, bot);
 
 		message.channel.send({
 			embeds: [
 				new MessageEmbed()
-					.setAuthor(`Скин игрока ${bot.utils.escapeMarkdown(data.name)}`, `https://crafatar.com/renders/head/${data.id}`)
+					.setAuthor(
+						`Скин игрока ${bot.utils.escapeMarkdown(data.name)}`,
+						`https://crafatar.com/renders/head/${data.id}`,
+					)
 					.setDescription(`[Скачать скин](https://crafatar.com/skins/${data.id})`)
 					.setImage(`https://crafatar.com/renders/body/${data.id}`),
 			],
