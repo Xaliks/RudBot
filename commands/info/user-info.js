8ca98dbd1ede7f22dae9a8e366b65e8f7c169e90
Xaliks <miskinmatvej77@gmail.com>
2021-10-07 20:06:32 +0300

    (commands/user-info): Фикс `Cannot find module`

--- commands/info/user-info.js
+++ commands/info/user-info.js
@@ -68,7 +68,7 @@ module.exports = {
 				if (!act) return;
 				if (act.id === "custom") status = act.state;
 				else {
-					activity += require("../../utils/msg/gamesEmoji")(act);
+					activity += require("../../utils/commands/msg/gamesEmoji")(act);
 				}
 			});
 			//-----------------------------------------------------------------------------
