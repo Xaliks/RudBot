482dfbb07c52a908ee2a0dc1a65d2236add6b90d
Xaliks <miskinmatvej77@gmail.com>
2021-10-14 14:14:02 +0300

    (commands/calc): Новая команда! :tada:

---
+++ commands/commands/calc.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+	name: "calc",
+	description: "Калькулятор",
+	cooldown: 5,
+	usage: ["<Пример>"],
+	category: "commands",
+	async execute(message, args, bot) {
+		const data = await fetch("http://api.mathjs.org/v4/", {
+            method: "POST",
+            body: JSON.stringify(new Object({
+                expr: args.join(" ")
+            }))
+        }).then(resp => resp.json())
+
+        if (!data.result) {
+            if (data.error.startsWith("Error: Undefined symbol ")) return bot.utils.error(`Неизвестный символ \`${data.error.slice(24)}\``, this, message, bot)
+            if (data.error.startsWith("Error: Unexpected operator ")) return bot.utils.error(`Неожиданный оператор \`${data.error.slice(27).split(" ")[0]}\` (символ \`${data.error.split("char ").pop().replace(")", "")}\`)`, this, message, bot)
+            if (data.error.startsWith("Error: Value expected (char ")) return bot.utils.error(`Неизвестное значение (символ \`${data.error.slice(28).replace(")", "")}\`)`, this, message, bot)
+
+            return bot.utils.error(data.error, this, message, bot)
+        }
+
+        message.channel.send({ embeds: [new MessageEmbed().setTitle("Калькулятор").setDescription(`Ответ: **${data.result}**`)]})
+	},
+};
