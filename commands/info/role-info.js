fe6d835ed5d77b08fa999e590377185faf5df778
Xaliks <miskinmatvej77@gmail.com>
2021-10-24 14:51:44 +0300

    (commands/role-info): Убрал права роли Когда discord.js уберут права с меткой `deprecated`, я добавлю снова

--- commands/info/role-info.js
+++ commands/info/role-info.js
@@ -1,10 +1,9 @@
 const { MessageEmbed } = require("discord.js");
-const { permissions } = require("../../data/data.json")
 
 module.exports = {
 	name: "role-info",
 	description: "Информация о роли",
-	aliases: ["role", "роль", "roleinfo", "инфороль", "роль-инфо", "ri"],
+	aliases: ["role", "roleinfo", "ri"],
 	usage: ["<Роль/ID>"],
 	cooldown: 5,
 	category: "info",
@@ -15,21 +14,20 @@ module.exports = {
 			message.guild.roles.cache.find((role) => role.id === args[0]) ||
 			message.guild.roles.cache.find((role) => role.name.toLowerCase().startsWith(args.join(" ").toLowerCase()));
 		if (!role) return bot.utils.error("Не могу найти роль!", this, message, bot);
-
+		
 		message.channel.send({
 			embeds: [
 				new MessageEmbed()
 					.setAuthor(role.name, role.iconURL({ format: "png" }))
 					.setColor(role.color)
-					.addField("**ID**", role.id, true)
-					.addField("**Упоминание**", `\`${role}\` / ${role}`, true)
-					.addField("**Пользователей с этой ролью:**", role.members.size, true)
-					.addField("**Цвет**", role.hexColor.toUpperCase(), true)
-					.addField("**Позиция:**", `${bot.utils.formatNumber(message.guild.roles.cache.size - role.position)}`, true)
-					.addField("**Отображаемая роль?**", role.hoist ? "Да" : "Нет", true)
-					.addField("**Упоминается?**", role.mentionable ? "Да" : "Нет", true)
-					.addField("**Создана**", bot.utils.discordTime(data.createdTimestamp), true)
-					.addField("Права", role.permissions.toArray().map(r => `**${permissions[r]}**`).join(", "))
+					.addField("ID", role.id, true)
+					.addField("Упоминание", `\`${role}\` / ${role}`, true)
+					.addField("Пользователей с этой ролью:", String(role.members.size), true)
+					.addField("Цвет", role.hexColor.toUpperCase(), true)
+					.addField("Позиция:", `${bot.utils.formatNumber(message.guild.roles.cache.size - role.position)}`, true)
+					.addField("Отображаемая роль?", role.hoist ? "Да" : "Нет", true)
+					.addField("Упоминается?", role.mentionable ? "Да" : "Нет", true)
+					.addField("Создана", bot.utils.discordTime(role.createdTimestamp), true)
 					.setFooter(message.author.username),
 			],
 		});
