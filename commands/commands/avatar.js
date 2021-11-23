2a9147ed60e2d7a265fac1bb9f19511129ac6658
Xaliks <miskinmatvej77@gmail.com>
2021-11-23 15:30:38 +0300

    (commands/avatar): TODO

--- commands/commands/avatar.js
+++ commands/commands/avatar.js
@@ -1,9 +1,5 @@
 ﻿const { MessageEmbed } = require("discord.js");
 
-/**
- * TODO: Сделать проверку на анимированную аву
- * TODO: Добавить серверный аватар
- */
 module.exports = {
 	name: "avatar",
 	description: "Ава пользователя",
@@ -12,25 +8,18 @@ module.exports = {
 	usage: ["[@Пользователь/ID]"],
 	async execute(message, args, bot) {
 		const user = await bot.users.fetch(args[0]).catch(() => bot.utils.findMember(message, args.join(" "), true).user);
+		const formats = ["webp", "png", "jpg"];
+		if (user.avatar.startsWith("a_")) formats.push("gif")
 
 		message.channel.send({
 			embeds: [
 				new MessageEmbed()
 					.setAuthor(bot.utils.escapeMarkdown(user.tag))
 					.setDescription(
-						`[PNG](${user.displayAvatarURL({
+						formats.map((format) => `[${format.toUpperCase()}](${user.displayAvatarURL({
 							size: 2048,
-							dynamic: true,
-							format: "png",
-						})}) / [JPG](${user.displayAvatarURL({
-							size: 2048,
-							dynamic: true,
-							fomat: "jpg",
-						})}) / [GIF](${user.displayAvatarURL({
-							size: 2048,
-							dynamic: true,
-							fomat: "gif",
-						})}) `,
+							format,
+						})})`).join(" / ")
 					)
 					.setImage(
 						user.displayAvatarURL({
