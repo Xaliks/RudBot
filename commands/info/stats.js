8920f5acc355b9227a3214144b8806b3b1ac064b
Xaliks <miskinmatvej77@gmail.com>
2022-01-27 21:04:40 +0300

    Убрал эмодзи, изменил `stats`

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -1,7 +1,6 @@
 const { MessageEmbed, version } = require("discord.js");
 const { owners, lavacordNodes } = require("../../config.json");
 const os = require("os");
-const { emojis } = require("../../data/data.json");
 
 module.exports = {
 	name: "stats",
@@ -32,32 +31,23 @@ module.exports = {
 					.setDescription(
 						`**Создатель:** \`${bot.utils.escapeMarkdown(await bot.users.fetch(owners[0]).then((owner) => owner.tag))}\`
 **Создан:** ${bot.utils.discordTime(bot.user.createdTimestamp, true, false)[0]}
-**[Пригласить меня](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot)**
-
-**Кол-во команд:** \`${bot.commands.filter((cmd) => !cmd.admin).size}\`
+**[Пригласить меня](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot)**`,
+					)
+					.addField("Статистика", `**Кол-во команд:** \`${bot.commands.filter((cmd) => !cmd.admin).size}\`
 **Пользователей:** \`${bot.utils.formatNumber(bot.users.cache.size)}\`
 **Серверов:** \`${bot.utils.formatNumber(bot.guilds.cache.size)}\`
-**Каналов:** \`${bot.utils.formatNumber(bot.channels.cache.size)}\`:
-(\`${channelSize("GUILD_CATEGORY")}\`кат. / \`${channelSize("GUILD_TEXT")}\`текст. / \`${channelSize(
-							"GUILD_VOICE",
-						)}\`голос.)
-
-${emojis.CPU}**Кол-во ядер процессора:** \`${os.cpus().length}\`
-${emojis.RAM}**Использование ОЗУ:** \`${(process.memoryUsage().rss / Math.pow(1024, 2)).toFixed(0)}\`MB / \`${(
-							os.totalmem() / Math.pow(1024, 3)
-						).toFixed(1)}\`GB
-${emojis.NodeJS}**Версия Node.js:** \`${process.version}\`
-${emojis.DiscordJS}**Версия Discord.js:** \`${version}\`
-${emojis.Linux}**Операционная система:** \`${os.type()} / ${os.arch()}\``,
-					)
+**Каналов:** \`${bot.utils.formatNumber(bot.channels.cache.size)}\``, true)
+					.addField("Система", `**ОЗУ:** \`${(process.memoryUsage().rss / Math.pow(1024, 2)).toFixed(0)}\`MB / \`${(
+		os.totalmem() / Math.pow(1024, 3)
+	).toFixed(1)}\`GB
+**Node.js** \`${process.version}\`
+**Discord.js** \`${version}\`
+**OC** \`${os.type()} / ${os.arch()}\``, true)
 					.setFooter({ text: `Аптайм: ${bot.utils.time(bot.uptime)}\nПоследняя перезагрузка была:` })
 					.setTimestamp(Date.now() - bot.uptime)
 					.setThumbnail(bot.user.displayAvatarURL()),
 				musicEmbed,
 			],
 		});
-		function channelSize(ChannelType) {
-			return bot.channels.cache.filter((c) => c.type === ChannelType).size;
-		}
 	},
 };
