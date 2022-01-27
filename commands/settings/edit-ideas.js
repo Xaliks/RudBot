342de9ddc2ef399d1f96eb5ab143fb5027649fda
Xaliks <miskinmatvej77@gmail.com>
2022-01-27 21:24:41 +0300

    Фикс

--- commands/settings/edit-ideas.js
+++ commands/settings/edit-ideas.js
@@ -12,7 +12,7 @@ module.exports = {
 
 		await bot.cache.delete({ id: message.guild.id }, "idea_channel", "guild");
 
-		if (!["channel", "role"].includes(type)) return bot.utils.error(`Правильное использование команды: \`${guild.prefix}${this.name} ${this.usage.join(" ")}\``)
+		if (!["channel", "role"].includes(type)) return bot.utils.error(`Правильное использование команды: \`${guild.prefix}${this.name} ${this.usage.join(" ")}\``, this, message, bot)
 
 		if (type.toLowerCase() === "channel") {
 			if (!options?.[0]) return bot.utils.error("Канал не указан!", this, message, bot);
