566380cd6d56a7fe194b2a17069e456fd346959a
Xaliks <miskinmatvej77@gmail.com>
2022-02-12 19:13:51 +0300

    фикс

--- utils/functions/findMember.js
+++ utils/functions/findMember.js
@@ -1,14 +1,20 @@
 module.exports = async (message, query, author) => {
 	let result = message.mentions.members.first();
 
-	if (!result) {
+	if (!result && query) {
 		query = query.toString().toLowerCase();
-		await message.guild.members.fetch({ query, limit: 1, force: true, withPresences: true }).then((collection) => {
-			result = collection.first();
-		})
+
+		if (/\d{16,18}/.test(query)) {
+			result = await message.guild.members.fetch({ user: query, force: true, withPresences: true });
+		} else
+			await message.guild.members.fetch({ query, limit: 1, force: true, withPresences: true }).then((collection) => {
+				result = collection.first();
+			});
 	}
 
-	if (!result && author) return message.member;
+	if (author) {
+		if (!query || !result) return message.member;
+	}
 
 	return result;
 };
