28d52012cd3433276ab3555289048ae970e8c51b
Xaliks <miskinmatvej77@gmail.com>
2022-02-12 18:56:23 +0300

    Переписал `minecraft`

--- commands/info/minecraft.js
+++ commands/info/minecraft.js
@@ -1,9 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 
-/**
- * TODO: Переписать здесь ВСЁ
- */
 module.exports = {
 	name: "minecraft",
 	description: "Инфо о minecraft сервере",
@@ -12,37 +9,26 @@ module.exports = {
 	category: "info",
 	aliases: ["майнкрафт", "мсервер"],
 	async execute(message, args, bot) {
-		const IP = args[0];
-		const data = await fetch(`https://api.mcsrvstat.us/2/${encodeURIComponent(IP)}`).then((res) => res.json());
+		const data = await fetch(`https://api.mcsrvstat.us/2/${encodeURIComponent(args.join(""))}`).then((res) => res.json());
 		if (data.ip === "") return bot.utils.error("IP не найден!", this, message, bot);
+
+		const embed = new MessageEmbed()
+			.setAuthor({ name: (data.hostname ? data.hostname + " | " : "") + data.ip + data.port, iconURL: `https://cdn.discordapp.com/emojis/${data.online ? "936319696058339348" : "784420488436514856"}.png` })
+			.setThumbnail(`https://api.mcsrvstat.us/icon/${args.join("")}`)
+			.setDescription("**СЕРВЕР ОФФЛАЙН**")
+
+		if (data.online) {
+			embed.description = `**Игроков: [\`${bot.utils.formatNumber(data.players?.online || 0)}\`/\`${bot.utils.formatNumber(data.players?.max || 0)}\`]**`;
+
+			if (data.motd) embed.description += `\`\`\`\n${data.motd?.clean.join("\n") || ""}\`\`\``
+
+			embed.setFooter({ text: "Версия: " + data.version })
+		}
 
 		message.channel.send({
 			embeds: [
-				new MessageEmbed()
-					.setTitle(`Информация о minecraft сервере`)
-					.setDescription(
-						`Онлайн? **${data.online ? "Да" : "Нет"}**
-
-**IP:** **${data.ip} ${data.hostname ? `/ ${data.hostname}` : ""}**
-**Порт:** **${data.port}**
-${
-	data.online
-		? `**Игроков:** \`${bot.utils.formatNumber(data.players.online)}\` **/** \`${bot.utils.formatNumber(
-				data.players.max,
-		  )}\`
-**Версия:** **${data.version}**
-
-${
-	data.motd.clean
-		? `**Описание:**
-\`${bot.utils.escapeMarkdown(data.motd.clean)}\``
-		: ""
-}`
-		: ""
-}`,
-					)
-					.setThumbnail(data.online ? `https://api.mcsrvstat.us/icon/${IP}` : ""),
-			],
-		});
+				embed
+			]
+		})
 	},
 };
