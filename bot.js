09f0bfbcbca68b0d6628926bc74c14cc59753c86
Xaliks <miskinmatvej77@gmail.com>
2022-02-12 19:26:42 +0300

    Фикс `findMember`

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const { token } = require("./config.json");
 const bot = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MEMBERS,
 		Intents.FLAGS.GUILD_MESSAGES,
 		Intents.FLAGS.GUILD_PRESENCES,
 		Intents.FLAGS.GUILD_VOICE_STATES,
--- utils/functions/findMember.js
+++ utils/functions/findMember.js
@@ -1,20 +1,16 @@
 module.exports = async (message, query, author) => {
-	let result = message.mentions.members.first();
+	const mention = message.mentions.members.first();
+	if (mention) return mention;
 
-	if (!result && query) {
-		query = query.toString().toLowerCase();
-
+	if (query) {
 		if (/\d{16,18}/.test(query)) {
-			result = await message.guild.members.fetch({ user: query, force: true, withPresences: true });
-		} else
-			await message.guild.members.fetch({ query, limit: 1, force: true, withPresences: true }).then((collection) => {
-				result = collection.first();
-			});
+			return await message.guild.members.fetch({ user: query, force: true, withPresences: true });
+		} else {
+			return await message.guild.members
+				.fetch({ query, limit: 1, force: true, withPresences: true })
+				.then((collection) => collection.first());
+		}
 	}
 
-	if (author) {
-		if (!query || !result) return message.member;
-	}
-
-	return result;
+	if (author) return message.member;
 };
