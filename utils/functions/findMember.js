567cc3fe546727e9db428ae23fe2fda5682b2a9c
Xaliks <miskinmatvej77@gmail.com>
2022-02-15 21:00:53 +0300

    Фикс `findMember`
    Не находится тег

--- utils/functions/findMember.js
+++ utils/functions/findMember.js
@@ -5,6 +5,10 @@ module.exports = async (message, query, author) => {
 	if (query) {
 		if (/\d{16,18}/.test(query)) {
 			return await message.guild.members.fetch({ user: query, force: true, withPresences: true });
+		} else if (/\S+#\d{4}$/.test(query)) {
+			return await message.guild.members
+				.fetch({ force: true, withPresences: true })
+				.then((colection) => colection.find((member) => member.user.tag.toLowerCase() === query.toLowerCase()));
 		} else {
 			return await message.guild.members
 				.fetch({ query, limit: 1, force: true, withPresences: true })
