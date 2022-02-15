8486611a9eb2660d5b5179002e31cd58c4c3506d
Xaliks <miskinmatvej77@gmail.com>
2022-02-15 17:15:45 +0300

    Ошибка теперь отображается нормально

--- utils/functions/sendError.js
+++ utils/functions/sendError.js
@@ -4,8 +4,10 @@ const { MessageEmbed } = require("discord.js");
 module.exports = (bot, error) => {
 	if (!error) return;
 
+	console.error(error);
+
 	bot.channels.cache.get(errorLogsChannelId).send({
 		content: "<@448799481777881089>",
-		embeds: [new MessageEmbed().setDescription(JSON.stringify(error))],
+		embeds: [new MessageEmbed().setDescription(JSON.stringify(error) || error)],
 	});
 };
