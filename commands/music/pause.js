403d0a8b52ad79f4407199f5301fa6269d2b5d6d
Xaliks <miskinmatvej77@gmail.com>
2022-02-24 13:24:07 +0300

    `pause`, `resume`

---
+++ commands/music/pause.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+	name: "pause",
+	description: "Приостановить вопроизведение",
+	category: "music",
+	async execute(message, args, bot) {
+		const player = bot.music.players.get(message.guild.id);
+		if (!player || player.queue.length === 0) return bot.utils.error("Очередь сервера пуста!", this, message, bot);
+		if (message.guild.me.voice.channelId && message.guild.me.voice.channelId != message.member.voice?.channelId)
+			return bot.utils.error(
+				`Бота использует кто-то другой! Зайдите в канал <#${message.guild.me.voice.channelId}>!`,
+				this,
+				message,
+				bot,
+			);
+		if (!player.state.playing) return bot.utils.error("Воспроизведение уже приостановлено!", this, message, bot);
+
+		await player.pause();
+
+		return message.channel.send({
+			embeds: [
+				new MessageEmbed()
+					.setTitle("🎶 Воспроизвение")
+					.setDescription(
+						`Воспроизведение было приостановлено участником **${bot.utils.escapeMarkdown(message.author.username)}** ⏹️`,
+					),
+			],
+		});
+	},
+};
